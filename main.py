01a70f4f0ae6dbd40d7b489f299f5aefea943350
Taseen-Zuhayr <taseen.zuhayr.64@gmail.com>
2024-08-31 23:08:53 +0600

    This is my second activity.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print('Hello! My classmates. How are you?')
 print('I am fine.')
+kill = 20
+print('Your killcount is',kill) 
+
+name = input( 'Enter name: ') 
+print("Hey",name'It has been a long time since you last visited.')
